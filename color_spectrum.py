997cc91faaba2e759d0eec61534fa725ae171cfe
Matt Brady <matthew.r.brady4@gmail.com>
2021-11-13 22:18:47 -0500

    refactor function ordering and naming

--- color_spectrum.py
+++ color_spectrum.py
@@ -1,13 +1,22 @@
-from tkinter import *
-from Color import Color
 import math
-
-
 import time
 
-# importing the choosecolor package
 from tkinter import colorchooser
 from types import SimpleNamespace
+from tkinter import *
+from Color import Color
+
+# TODO: look into making a colorspace class
+# From here on out, meant to be read by function from bottom to top
+
+def update_ns_and_print(window_ns, colorspace, dimX, dimY):
+    print("\n*\n*\n*\n*\tNew Color matrix!\n*\n*\n*\n")
+    for i in range(dimY):
+        print("Row " + str(i+1) + "\n")
+        for j in range(dimX):
+            window_ns[i][j].color = colorspace[i][j]
+            print("Row " + str(i+1) +" Column " + str(j+1) + " color: " + str(colorspace[i][j]))
+        print("\n")
 
 def make_averaged_row_from_endpoints(color_1, color_2, num_segments):
     return [get_proportion_for_2_colors(color_1, color_2, i, num_segments-1) for i in range(num_segments)]
@@ -19,42 +28,36 @@ def get_proportion_for_2_colors(color_1, color_2, numerator, denominator):
     linear_proportion = color_1 - proportional_color
     return linear_proportion
 
-# algo for calculating a 2d colorspace, from the 4 points outlining the boundary
+# algo for calculating a 2d colorspace, from the colors of 4 corners outlining the boundary
 # input: inputs 1-4 are each a Color, 5 is matrix width, 6 is the height
 # output: list of lists in row-major order, so inputListOfLists[row][column]
-def calculate_2d_colorspace_from_corners(topLeftColor, topRightColor, btmLeftColor, btmRightColor, dimX, dimY):
+def calculate_2d_colorspace_from_corners(topLeftColor, topRightColor, btmLeftColor, btmRightColor, widthX, heightY):
     lookAtLast=False
-    inputListOfLists = [ [Color() for x in range(dimX)] for y in range(dimY)]
+    inputListOfLists = [ [Color() for x in range(widthX)] for y in range(heightY)]
     # for every row in the matrix, calculate the proportional left&right side, then fill it in
-    for row_num in range(dimY):
-        left_proportion = get_proportion_for_2_colors(topLeftColor, btmLeftColor, row_num, dimY)
-        right_proportion = get_proportion_for_2_colors(topRightColor, btmRightColor, row_num, dimY)
-        inputListOfLists[row_num] = make_averaged_row_from_endpoints(left_proportion, right_proportion, dimX)
+    for row_num in range(heightY):
+        left_proportion = get_proportion_for_2_colors(topLeftColor, btmLeftColor, row_num, heightY)
+        right_proportion = get_proportion_for_2_colors(topRightColor, btmRightColor, row_num, heightY)
+        inputListOfLists[row_num] = make_averaged_row_from_endpoints(left_proportion, right_proportion, widthX)
     return inputListOfLists
 
-def map_window_ns_to_new_colorspace(window_ns):
-    size_y = len(window_ns)
-    size_x = len(window_ns[0])
+def map_window_ns_to_new_colorspace_and_print(window_ns):
+    # accessed this way to ensure that the corners are valid values at the correct positions
+    # as opposed to the pattern through the rest of this code, which is passing variables
+    # tradeoff: lookup is a bit slower to access
+    dimY = len(window_ns)
+    dimX = len(window_ns[0])
     top_left = window_ns[0][0].color
-    top_right =window_ns[0][size_x-1].color
-    bottom_left = window_ns[size_y-1][0].color
-    bottom_right =window_ns[size_y-1][size_x-1].color
-    colorspace = calculate_2d_colorspace_from_corners(top_left, top_right, bottom_left, bottom_right, size_x, size_y)
-    for i in range(size_y):
-        print("Row " + str(i+1) + "\n")
-        for j in range(size_x):
-            window_ns[i][j].color = colorspace[i][j]
-            print("Row " + str(i+1) +" Column " + str(j+1) + " color: " + str(colorspace[i][j]))
-        print("\n")
-
-
-def is_corner(x,y,dimX,dimY):
-    return ((x == 0 or x == dimX-1) and (y == 0 or y == dimY-1))
+    top_right =window_ns[0][dimX-1].color
+    bottom_left = window_ns[dimY-1][0].color
+    bottom_right =window_ns[dimY-1][dimX-1].color
+    colorspace = calculate_2d_colorspace_from_corners(top_left, top_right, bottom_left, bottom_right, dimX, dimY)
+    update_ns_and_print(window_ns, colorspace, dimX, dimY)
 
 def change_color(window_ns, ind_Y, ind_X, dimX, dimY):
     colors = colorchooser.askcolor(title="Tkinter Color Chooser")
     window_ns[ind_Y][ind_X].color = Color.init_from_tkinter_hex(colors[1])
-    map_window_ns_to_new_colorspace(window_ns)
+    map_window_ns_to_new_colorspace_and_print(window_ns)
 
 def make_window(window_ns, ind_Y, ind_X, dimY, dimX):
     new_window = Tk(className=window_ns[ind_Y][ind_X].name)
@@ -63,6 +66,18 @@ def make_window(window_ns, ind_Y, ind_X, dimY, dimX):
         Button(new_window, text='Select a Color',
             command=lambda: change_color(window_ns, ind_Y, ind_X, dimX, dimY)).pack(expand=True)
     return new_window
+
+def make_window_geometry(screen_width, screen_height, dimX, dimY, x, y):
+    window_width = math.floor(screen_width/dimX)
+    # minus 40 accounts for the bar at the top of the window
+    window_height = math.floor(screen_height/dimY)-40
+    # example geometry:'300x200+640+250', which is xdim,ydim,xoffset,yoffset
+    window_size = "" + str(window_width) + "x" + str(window_height) + "+"
+    geometry = window_size + str(window_width * x) + "+" + str(window_height * y)
+    return geometry
+
+def is_corner(x,y,dimX,dimY):
+    return ((x == 0 or x == dimX-1) and (y == 0 or y == dimY-1))
 
 def get_window_name(x,y,dimX,dimY):
     if is_corner(x,y,dimX,dimY):
@@ -78,17 +93,10 @@ def get_window_name(x,y,dimX,dimY):
     else:
         return "Spectrum Row " + str(x) + " Column " + str(y)
 
-def make_window_geometry(screen_width, screen_height, dimX, dimY, x, y):
-    window_width = math.floor(screen_width/dimX)
-    # minus 40 accounts for the bar at the top of the window
-    window_height = math.floor(screen_height/dimY)-40
-    # example geometry:'300x200+640+250'
-    window_size = "" + str(window_width) + "x" + str(window_height) + "+"
-    geometry = window_size + str(window_width * x) + "+" + str(window_height * y)
-    return geometry
-
 
 def _main():
+
+    print("This program may take a second to initialize :)")
 
     # number of windows on the screen in each dimension
     dimX = 16
@@ -97,6 +105,7 @@ def _main():
     screen_height = 1080
 
     # array of namespaces, will be populated with the following attrs
+    # using namespaces allows for pass-by-reference
     window_ns = [ [SimpleNamespace() for x in range(dimX)] for y in range(dimY)]
     window_names= [ [get_window_name(x,y,dimX,dimY) for x in range(dimX)] for y in range(dimY)]
     window_is_input=[ [is_corner(x, y,dimX,dimY) for x in range(dimX)] for y in range(dimY)]
--- readme.md
+++ readme.md
@@ -1,18 +1,13 @@
-## Color Math
+## Color spectrum  
 
-This python program to visually creates a spectrum chosen from color pickers.
-Input order is labeled in the window name, and you can look up the RGB values in the console window.
-Close one window to close all of them, but all the color pickers must be closed first.
+This python program visually calculates a 2D spectrum of colors in between 4 user-choosable options!  
+Also displays the RGB values in the console. click on a window to get its index to look it up, although it might have to expanded
 
-run with py color_spectrum.py
 
-## Color spectrum
+run with `py color_spectrum.py`  
 
-This python program visually calculates a 2D spectrum of colors in between 4 user-choosable options!
-
-In the code:
-dimX is the number of colors(windows) horizontally across the screen, including inputs
-dimY is the number of colors(windows) vertically down the screen, including inputs
-screen_width is the number of pixels you want the orchestrated windows to take up horizontally
-screen_height is the number of pixels you want the orchestrated windows to take up vertically
-the actual size of each window is pretty much just (screen_width / dimX) by (screen_height / dimY)
+In the code:  
+`dimX` is the number of colors(windows) horizontally across the screen, including inputs  
+`dimY` is the number of colors(windows) vertically down the screen, including inputs  
+`screen_width` is the number of pixels you want the orchestrated windows to take up horizontally  
+`screen_height` is the number of pixels you want the orchestrated windows to take up vertically  
